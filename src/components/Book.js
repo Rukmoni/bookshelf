b6d7a21b64f665c82ed9bff7b889f79768b51f34
Rukmoni <rukmoni.be@gmail.com>
2021-04-12 15:14:07 +0800

    feat:Book component updated with api data

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,15 +2,17 @@ import React from 'react';
 import OptionMenu from './OptionMenu';
 import '../App.css'
 
-const Book=()=>{
+const Book=({data})=>{
+  console.log("auther",data.authors[0])
     return(
         <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+         {/*  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div> */}
+         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${data.imageLinks.thumbnail})` }}></div>
         <OptionMenu/>
         </div>
-        <div className="book-title">To Kill a Mockingbird</div>
-        <div className="book-authors">Harper Lee</div>
+        <div className="book-title">{data.title}</div>
+        <div className="book-authors">{data.authors[0]}</div>
       </div>
 
     )
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import Book from './Book';
-import '../App.css'
+import '../App.css';
 
-const BookShelf=({shelf,booksList})=>{
-    return(
-        <div className="bookshelf">
-        <h2 className="bookshelf-title">{shelf.title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            <li>
-              <Book/>
-            </li>
-            <li>
-           <Book/>
-            </li>
-          </ol>
-        </div>
-      </div>
-
-    )
-}
+const BookShelf = ({ shelf, booksList }) => {
+	return (
+		<div className="bookshelf">
+			<h2 className="bookshelf-title">{shelf.title}</h2>
+			<div className="bookshelf-books">
+				<ol className="books-grid">
+					{booksList &&
+						booksList.map((book) => {
+							return (
+								<li>
+									<Book data={book}/>
+								</li>
+							);
+						})}
+				</ol>
+			</div>
+		</div>
+	);
+};
 
 export default BookShelf;
