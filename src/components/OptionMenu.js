95145b8a3537344aa1db138de7b6b080fbba8cf5
Rukmoni <rukmoni.be@gmail.com>
2021-04-22 08:58:33 +0800

    Fix: Added None as another shelf option, and get selected when no other shelf selected

--- src/components/OptionMenu.js
+++ src/components/OptionMenu.js
@@ -1,30 +1,41 @@
-import React from 'react';
-import {SHELFS} from '../constants';
+import React, { useState, useEffect } from 'react';
+import { SHELFS } from '../constants';
 import PropTypes from 'prop-types';
-import '../App.css'
+import '../App.css';
 
-const OptionMenu=({shelfChange,book,currentShelf})=>{
-  function changeShelf(evt)
-  {
-   evt.preventDefault();
-  shelfChange(book,evt.target.value)
-  }
-    return(
-        <div className="book-shelf-changer">
-        <select onChange={(e)=>changeShelf(e)} value={currentShelf} >
-          <option value={SHELFS.CURRENTLY_READING_SHELF.key}>Currently Reading</option>
-          <option value={SHELFS.WANT_TO_READ_SHELF.key}>Want to Read</option>
-          <option value={SHELFS.READ_SHELF.key}>Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>
-    
+const OptionMenu = ({ shelfChange, book, currentShelf }) => {
+	const [selectedShelf, setSelectedShelf] = useState(currentShelf);
 
-    )
-}
-OptionMenu.protoTypes={
-  data:PropTypes.array,
-  shelfChange:PropTypes.func,
-  currentShelf:PropTypes.string
-}
+	useEffect(() => {
+    console.log("currentShelf",currentShelf);
+		if (typeof(currentShelf) != "undefined") {
+      
+      setSelectedShelf(currentShelf);
+    }else
+    {
+      console.log("Shelf");
+      setSelectedShelf(SHELFS.None);
+    }
+	}, []);
+	function changeShelf(evt) {
+    evt.preventDefault();
+    setSelectedShelf(evt.target.value)
+		shelfChange(book, evt.target.value);
+	}
+	return (
+		<div className="book-shelf-changer">
+			<select onChange={(e) => changeShelf(e)} value={selectedShelf}>
+				<option value={SHELFS.CURRENTLY_READING_SHELF.key}>Currently Reading</option>
+				<option value={SHELFS.WANT_TO_READ_SHELF.key}>Want to Read</option>
+				<option value={SHELFS.READ_SHELF.key}>Read</option>
+				<option value={SHELFS.None}>None</option>
+			</select>
+		</div>
+	);
+};
+OptionMenu.protoTypes = {
+	data: PropTypes.array,
+	shelfChange: PropTypes.func,
+	currentShelf: PropTypes.string,
+};
 export default OptionMenu;
--- src/constants.js
+++ src/constants.js
@@ -6,5 +6,6 @@
 export const SHELFS={
     CURRENTLY_READING_SHELF:{key:"currentlyReading", title:'Currently Reading'},
     WANT_TO_READ_SHELF:{key:"wantToRead", title:'Want To Read'},
-    READ_SHELF:{key:"read", title:'Read'}
+    READ_SHELF:{key:"read", title:'Read'},
+    None:{key:"none", title:'None'}
 }
