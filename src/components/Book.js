6af64ca98a4df7ae198704d6b24ffdd5cba8a4fc
Rukmoni <rukmoni.be@gmail.com>
2021-04-22 08:33:39 +0800

    Fix: Displaying all author names

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import OptionMenu from './OptionMenu';
 import PropTypes from 'prop-types';
-import '../App.css'
+import '../App.css';
 
-const Book=({data,shelfChange})=>{
-    return(
-        <div className="book">
-        <div className="book-top">
-         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${data.imageLinks.thumbnail})` }}></div>
-        <OptionMenu shelfChange={shelfChange} book={data} currentShelf={data.shelf}/>
-        </div>
-        <div className="book-title">{data.title}</div>
-       <div className="book-authors">{data.authors&&data.authors.length>0?data.authors[0]:''}</div> 
-      </div>
-
-    )
-}
-Book.protoTypes={
-  data:PropTypes.array,
-  shelfChange:PropTypes.func,
-}
+const Book = ({ data, shelfChange }) => {
+	const getAuthorsName = () => {
+		return (
+      data.authors&&data.authors.length>0?
+			<div className="book-authors">
+      {data.authors.map((author)=>
+				<div>{author}</div>
+      )}
+			</div>:<div></div>
+		);
+	};
+	return (
+		<div className="book">
+			<div className="book-top">
+				<div
+					className="book-cover"
+					style={{ width: 128, height: 193, backgroundImage: `url(${data.imageLinks.thumbnail})` }}
+				></div>
+				<OptionMenu shelfChange={shelfChange} book={data} currentShelf={data.shelf} />
+			</div>
+			<div className="book-title">{data.title}</div>
+      {getAuthorsName()}
+		</div>
+	);
+};
+Book.protoTypes = {
+	data: PropTypes.array,
+	shelfChange: PropTypes.func,
+};
 export default Book;
